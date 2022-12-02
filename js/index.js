324ea8b407eab8e086600590f9b978bb8d2b2ec4
beharekonjuvca <117676424+beharekonjuvca@users.noreply.github.com>
2022-12-02 23:10:31 +0100

    update

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const add = document.querySelector(".add");
+function toggleActive() {
+  add.classList.toggle("active");
+}
+function unToggle() {
+  add.classList.replace("active", "nonactive");
+}
